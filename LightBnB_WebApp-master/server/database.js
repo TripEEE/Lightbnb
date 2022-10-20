24e8ff047c9f3c73da4de0e2f045855fac2b5065
TripEEE <evan.trippel@gmail.com>
2022-10-20 14:07:42 -0400

    Refactored 4 functions to use lightbnb database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,3 +1,13 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  port: 5432,
+  database: 'lightbnb'
+})
+
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
@@ -8,29 +18,45 @@ const users = require('./json/users.json');
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+
+const getUserWithEmail = (email) => {
+  const toLowerCase = email.toLowerCase()
+  return pool.query(`
+    SELECT * FROM users
+    WHERE users.email = $1
+  `, [toLowerCase])
+    .then((result) => {
+      console.log(result.rows[0]);
+      return result.rows[0]
+    })
+    .catch((err) => {
+      console.log(err.message)
+      return null
+    })
 }
-exports.getUserWithEmail = getUserWithEmail;
+
 
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+
+const getUserWithId = (id) => {
+  return pool.query(`
+  SELECT * FROM users
+  WHERE users.id = $1
+  `, [id])
+    .then((result) => {
+      console.log(result.rows[0]);
+      return result.rows[0]
+    })
+    .catch((err) => {
+      console.log(err.message)
+      return null
+    })
 }
-exports.getUserWithId = getUserWithId;
+
 
 
 /**
@@ -38,13 +64,32 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+// const addUser = function (user) {
+//   const userId = Object.keys(users).length + 1;
+//   user.id = userId;
+//   users[userId] = user;
+//   return Promise.resolve(user);
+// }
+
+//
+const addUser = (name, email, password) => {
+  pool.query(`
+    INSERT INTO users 
+    (name, email, password) 
+    VALUES ($1, $2, $3)
+    RETURNING *; 
+  `, [name, email, password])
+    .then((result) => {
+      console.log(result.rows[0]);
+      return result.rows[0]
+    })
+    .catch((err) => {
+      console.log(err.message)
+      return null
+    })
 }
-exports.addUser = addUser;
+
+
 
 /// Reservations
 
@@ -56,7 +101,6 @@ exports.addUser = addUser;
 const getAllReservations = function (guest_id, limit = 10) {
   return getAllProperties(null, 2);
 }
-exports.getAllReservations = getAllReservations;
 
 /// Properties
 
@@ -66,14 +110,22 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
-}
-exports.getAllProperties = getAllProperties;
+
+const getAllProperties = (options, limit = 10) => {
+
+  return pool
+    .query(`
+      SELECT * FROM properties
+      LIMIT $1`,
+      [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 
 
 /**
@@ -87,4 +139,12 @@ const addProperty = function (property) {
   properties[propertyId] = property;
   return Promise.resolve(property);
 }
+
+addUser('Jon Fejtek', 'mrpoops@gmail.com', 'password')
+
+exports.getAllReservations = getAllReservations;
 exports.addProperty = addProperty;
+exports.getAllProperties = getAllProperties;
+exports.addUser = addUser;
+exports.getUserWithId = getUserWithId;
+exports.getUserWithEmail = getUserWithEmail;
